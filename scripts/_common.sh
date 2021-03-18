8a41e76b45f15b154bfaa43b98be66e1b02bf173
Georgelemental <georgelemental@gmail.com>
2021-03-17 23:45:54 -0400

    Initial commit for nitter package

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,11 +5,35 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="nim redis-server libsass-dev"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+# Check the architecture
+#
+# example: architecture=$(ynh_detect_arch)
+#
+# usage: ynh_detect_arch
+#
+# Requires YunoHost version 2.2.4 or higher.
+
+ynh_detect_arch(){
+	local architecture
+	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+		architecture="arm64"
+	elif [ -n "$(uname -m | grep 64)" ]; then
+		architecture="x86-64"
+	elif [ -n "$(uname -m | grep 86)" ]; then
+		architecture="i386"
+	elif [ -n "$(uname -m | grep arm)" ]; then
+		architecture="arm"
+	else
+		architecture="unknown"
+	fi
+	echo $architecture
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
