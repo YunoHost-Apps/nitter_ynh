8b7594a1188c9782953ff70a8795e597757063ca
Jules Bertholet <jules.bertholet@gmail.com>
2021-04-17 21:40:36 -0400

    Minor improvements to scripts

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,6 +11,28 @@ pkg_dependencies="acl nim redis-server libsass-dev"
 # PERSONAL HELPERS
 #=================================================
 
+function build_nitter {
+	pushd "$final_path" || ynh_die
+		chown -R $app:$app $final_path
+		sudo -u $app env "PATH=/opt/yunohost/$app/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble build -d:release -y
+		sudo -u $app env "PATH=/opt/yunohost/$app/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble scss -y
+		mkdir -p $final_path/tmp
+		chown -R root:root $final_path
+	popd || ynh_die
+}
+
+function set_permissions {
+	chown -R root:$app $final_path
+	chown -R $app:$app $final_path/tmp
+	chmod -R g=u,g-w,o-rwx $final_path
+	setfacl -n -m u:www-data:--x $final_path
+	setfacl -nR -m u:www-data:r-x -m d:u:www-data:r-x $final_path/public
+}
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
 # Check the architecture
 #
 # example: architecture=$(ynh_detect_arch)
@@ -34,10 +56,6 @@ ynh_detect_arch(){
 	fi
 	echo $architecture
 }
-
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
 
 #=================================================
 # FUTURE OFFICIAL HELPERS
