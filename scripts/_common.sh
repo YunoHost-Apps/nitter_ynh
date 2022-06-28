4508d48f466a4f1aa9a1ec77deca56aad4d7d79f
Jules Bertholet <julesbertholet@quoi.xyz>
2022-06-28 10:20:19 -0400

    Fix upgrade from old path

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,9 +14,9 @@ pkg_dependencies="acl nim libsass-dev"
 function build_nitter {
 	pushd "$final_path"
 		chown -R $app:$app $final_path
-		sudo -u $app env "PATH=/var/www/$app/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble build -d:release -y -d:danger --passC:"-flto" --passL:"-flto" 2>&1
-		sudo -u $app env "PATH=/var/www/$app/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" strip -s nitter  2>&1
-		sudo -u $app env "PATH=/var/www/$app/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble scss -y 2>&1
+		sudo -u $app env "PATH=$final_path/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble build -d:release -y -d:danger --passC:"-flto" --passL:"-flto" 2>&1
+		sudo -u $app env "PATH=$final_path/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" strip -s nitter  2>&1
+		sudo -u $app env "PATH=$final_path/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble scss -y 2>&1
 		mkdir -p $final_path/tmp
 		chown -R root:root $final_path
 	popd
