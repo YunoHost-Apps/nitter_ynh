7e8cf426886ac9ddbc6407578bd657f8939adf8f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-08 11:17:08 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,29 +5,29 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="acl nim libsass-dev"
+#REMOVEME? pkg_dependencies="acl nim libsass-dev"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
 function build_nitter {
-	pushd "$final_path"
-		chown -R $app:$app $final_path
-		sudo -u $app env "PATH=$final_path/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble build -d:release -y -d:danger --passC:"-flto" --passL:"-flto" 2>&1
-		sudo -u $app env "PATH=$final_path/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" strip -s nitter  2>&1
-		sudo -u $app env "PATH=$final_path/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble scss -y 2>&1
-		mkdir -p $final_path/tmp
-		chown -R root:root $final_path
+	pushd "$install_dir"
+		chown -R $app:$app $install_dir
+		sudo -u $app env "PATH=$install_dir/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble build -d:release -y -d:danger --passC:"-flto" --passL:"-flto" 2>&1
+		sudo -u $app env "PATH=$install_dir/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" strip -s nitter  2>&1
+		sudo -u $app env "PATH=$install_dir/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble scss -y 2>&1
+		mkdir -p $install_dir/tmp
+		chown -R root:root $install_dir
 	popd
 }
 
 function set_permissions {
-	chown -R root:$app $final_path
-	chown -R $app:$app $final_path/tmp
-	chmod -R g=u,g-w,o-rwx $final_path
-	setfacl -n -m u:www-data:--x $final_path
-	setfacl -nR -m u:www-data:r-x -m d:u:www-data:r-x $final_path/public
+	chown -R root:$app $install_dir
+	chown -R $app:$app $install_dir/tmp
+	chmod -R g=u,g-w,o-rwx $install_dir
+	setfacl -n -m u:www-data:--x $install_dir
+	setfacl -nR -m u:www-data:r-x -m d:u:www-data:r-x $install_dir/public
 }
 
 #=================================================
