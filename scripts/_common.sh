2d1bb46ea54c6401c803e00e860fd82ac6c2b48a
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-08 11:25:16 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="acl nim libsass-dev"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
